8953e7f55ae0a8ed7f381c2dbf8edca5b429d8fe
Ida Adjivon <iadjivon@bu.edu>
2021-01-07 02:09:43 -0500

    changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# this is a test to check for push to github
 
 
 Plant.create(
